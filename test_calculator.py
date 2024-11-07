c0910c7b819c80ad8618ce58073998f006550713
Chatra-Danai  Emphandhu <chatra.danai@gmail.com>
2024-11-07 12:50:48 +0700

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -6,10 +6,40 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
 
-    def test_add(self):
-        self.assertEqual(self.calc.add(1, 2), 3)
+    # Test cases for add method
+    def test_add_positive_numbers(self):
+        self.assertEqual(self.calc.add(10, 5), 15)
 
-    # Add the following test methods to the TestCalculator class:
+    def test_add_negative_numbers(self):
+        self.assertEqual(self.calc.add(-10, -5), -15)
+
+    # Test cases for subtract method
+    def test_subtract_positive_numbers(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+
+    def test_subtract_negative_numbers(self):
+        self.assertEqual(self.calc.subtract(-10, -5), 5)
+
+    # Test cases for multiply method
+    def test_multiply_positive_numbers(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+
+    def test_multiply_with_zero(self):
+        self.assertEqual(self.calc.multiply(3, 0), 0)
+
+    # Test cases for divide method
+    def test_divide_exact_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+
+    def test_divide_remainder(self):
+        self.assertEqual(self.calc.divide(9, 2), 4)  # Should return quotient
+
+    # Test cases for modulo method
+    def test_modulo_remainder(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+
+    def test_modulo_no_remainder(self):
+        self.assertEqual(self.calc.modulo(10, 5), 0)
 
 if __name__ == '__main__':
     unittest.main()
